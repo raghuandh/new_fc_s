7ec63bf3960b75f1a736da0b931b5bff55df05b2
raghunadh <miryalasai555@gmail.com>
2023-07-30 11:31:50 +0530

    another

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+
 #! /usr/bin/env python
+'''
 import os
 import cv2
 import argparse
@@ -49,3 +51,50 @@ if __name__ == '__main__':
         cv2.waitKey(0)
         
         cv2.destroyAllWindows()
+'''
+
+import os
+
+import numpy
+import argparse
+import streamlit as st
+from PIL import Image, ImageEnhance
+from face_detection import select_face, select_all_faces
+from face_swap import face_swap
+import cv2
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='FaceSwapApp')
+    parser.add_argument('--correct_color',default=True, action = 'store_true',help = 'Correct color')
+    parser.add_argument('--warp_2d',default=False, action='store_true',help = '2d or 3d warp')
+    args = parser.parse_args()
+    uploaded_scfile = st.file_uploader("Source File",type = ['jpg','png','jpeg'])
+    uploaded_tgfile = st.file_uploader("Target File",type = ['jpg','png','jpeg'])
+    
+    if uploaded_scfile is not None and uploaded_tgfile is not None:
+        source_image = Image.open(uploaded_scfile)
+        target_image = Image.open(uploaded_tgfile)
+        
+        src_img = cv2.cvtColor(numpy.array(source_image), cv2.IMREAD_COLOR)
+        dst_img = cv2.cvtColor(numpy.array(target_image), cv2.IMREAD_COLOR)
+    
+        src_points, src_shape, src_face = select_face(src_img)
+        
+        dst_faceBoxes = select_all_faces(dst_img)
+        
+        if dst_faceBoxes is None:
+            print('Detext 0 Face !!')
+            exit(-1)
+        
+        output = dst_img
+        
+        for k, dst_face in dst_faceBoxes.items():
+            output = face_swap(src_face, dst_face["face"],src_points,\
+                dst_face["points"],dst_face["shape"],output,args)
+            
+            
+            st.markdown('<p style = "text-align: left;">Result</p>',unsafe_allow_html=True)
+            
+            st.image(output, width=500)
+            
+                
